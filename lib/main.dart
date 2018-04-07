92d9a85fd475068de2f6e4b42cfd9542b308cf3f
nimit95 <nimitagg95@gmail.com>
2018-04-07 08:58:29 +0530

    Todo class and material theme
    Signed-off-by: nimit95 <nimitagg95@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,47 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget{
-  
-  
+class TodoItem {
+  const TodoItem({this.title, this.todoText});
+
+  final String title;
+  final String todoText;
+}
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'TodoApp',
-      home: new TodoList(),
+      //home: new TodoApp(),
+      theme: new ThemeData.dark(),
+    );
+  }
+}
+
+class TodoApp extends State {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Todo App'),
+      ),
+      body:,
+      floatingActionButton: floatingActionButton(),
     );
   }
 
+  Widget floatingActionButton() {
+    return new FloatingActionButton(
+      tooltip: "Add Todo",
+      onPressed: _onFABClick,
+      child: new Icon(Icons.add),
+    );
+  }
 
-}
+  void _onFABClick() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
 
-class TodoList extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
+    }));
   }
 
 }

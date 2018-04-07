505a15f66fe22617898c835f20212181470d1187
nimit95 <nimitagg95@gmail.com>
2018-04-07 11:18:12 +0530

    add todo dialog layout
    Signed-off-by: nimit95 <nimitagg95@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -8,42 +8,92 @@ class TodoItem {
   final String title;
   final String todoText;
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'TodoApp',
-      //home: new TodoApp(),
+      home: new MyTodoApp(),
       theme: new ThemeData.dark(),
     );
   }
 }
 
-class TodoApp extends State {
+class MyTodoApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new TodoApp();
+}
+
+class TodoApp extends State<MyTodoApp> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Todo App'),
       ),
-      body:,
+      // body:,
       floatingActionButton: floatingActionButton(),
     );
   }
 
   Widget floatingActionButton() {
+    String title, todoMsg;
+    Widget titleTextField = new TextField(
+      onSubmitted: (String t) {
+        title = t;
+      },
+      autofocus: true,
+      decoration: new InputDecoration(
+          labelText: 'Todo Title',
+          labelStyle: new TextStyle(
+              fontSize: 20.0, fontWeight: FontWeight.bold),
+              hintText: 'Complete Work'),
+    );
+
+    Widget todoMsgTextField = new TextField(
+      onSubmitted: (String t) {
+        todoMsg = t;
+      },
+      autofocus: true,
+      decoration: new InputDecoration(
+          labelText: 'Todo Details',
+          labelStyle: new TextStyle(
+              fontSize: 20.0, fontWeight: FontWeight.bold),
+          hintText: 'Have to be completed by next week'),
+    );
+
+    final SimpleDialog simpleDialog = new SimpleDialog(
+      title: new Text("Add Todo"),
+      contentPadding: new EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 25.0),
+      children: <Widget>[titleTextField,todoMsgTextField],
+    );
+
     return new FloatingActionButton(
       tooltip: "Add Todo",
-      onPressed: _onFABClick,
+      onPressed: () {
+        showDialog(
+            context: context,
+            child: new _SystemPadding(
+              child: simpleDialog,
+            ));
+      },
       child: new Icon(Icons.add),
     );
   }
-
-  void _onFABClick() {
-    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
-
-    }));
-  }
-
 }
 
+class _SystemPadding extends StatelessWidget {
+  final Widget child;
+
+  _SystemPadding({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
+    return new AnimatedContainer(
+        padding: mediaQuery.viewInsets,
+        duration: const Duration(milliseconds: 300),
+        child: child);
+  }
+}

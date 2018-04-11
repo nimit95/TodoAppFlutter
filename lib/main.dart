b17ea94ff3e8fd2da9ef300f962b82a0c90dc337
nimit95 <nimitagg95@gmail.com>
2018-04-11 20:44:18 +0530

    basic todo complete
    Signed-off-by: nimit95 <nimitagg95@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -26,13 +26,55 @@ class MyTodoApp extends StatefulWidget {
 }
 
 class TodoApp extends State<MyTodoApp> {
+  final Set<TodoItem> todos = new Set<TodoItem>();
+
   @override
   Widget build(BuildContext context) {
+    if (todos.isEmpty) {
+      return new Scaffold(
+          appBar: new AppBar(
+            title: new Text('Todo App'),
+          ),
+          body: new Center(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Icon(
+                  Icons.group_add,
+                  size: 150.0,
+                ),
+                new Text('Add Todos', style: new TextStyle(fontSize: 30.0),)
+              ],
+            ),
+          ),
+          floatingActionButton: floatingActionButton());
+    }
+    final todoListItems = todos.map((todo) {
+      return new ListTile(
+        title: new Text(
+          todo.title,
+          style: new TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: new Text(
+          todo.todoText,
+          style: new TextStyle(fontSize: 22.0),
+        ),
+      );
+    }).toList();
+
+    final divided =
+        ListTile.divideTiles(tiles: todoListItems, context: context).toList();
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Todo App'),
       ),
-      // body:,
+      body: new ListView(
+        children: divided,
+      ),
       floatingActionButton: floatingActionButton(),
     );
   }
@@ -40,33 +82,45 @@ class TodoApp extends State<MyTodoApp> {
   Widget floatingActionButton() {
     String title, todoMsg;
     final Widget titleTextField = new TextField(
-      onSubmitted: (String t) {
+      onChanged: (String t) {
         title = t;
       },
       autofocus: true,
       decoration: new InputDecoration(
           labelText: 'Todo Title',
           labelStyle:
-          new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
           hintText: 'Complete Work'),
     );
 
     final Widget todoMsgTextField = new TextField(
-      onSubmitted: (String t) {
+      onChanged: (String t) {
         todoMsg = t;
       },
       autofocus: true,
+      keyboardType: TextInputType.multiline,
+      maxLines: 3,
       decoration: new InputDecoration(
           labelText: 'Todo Details',
           labelStyle:
-          new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
           hintText: 'Have to be completed by next week'),
     );
+
+    void _addTodoItem(TodoItem todo) {
+      setState(() {
+        todos.add(todo);
+      });
+    }
 
     final submitButton = new FlatButton(
       onPressed: () {
         Navigator.of(context).pop(context);
         print(title + "Msg is " + todoMsg);
+
+        _addTodoItem(new TodoItem(title: title, todoText: todoMsg));
+        title = "";
+        todoMsg = "";
       },
       child: new Text("SUBMIT"),
       padding: new EdgeInsets.all(15.0),
